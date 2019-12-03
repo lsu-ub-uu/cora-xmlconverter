89d3f9e050ed36eaa9ea1633be2133bd5c781dcd
perebartrolisimo <pere.bartroli@ub.uu.se>
2019-12-03 15:39:15 +0000

    Added javadoc to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,24 @@
+/**
+ * This module provides an implementation for the converter interface converting between XML and
+ * {@link DataElement}.
+ * 
+ * This implementation maps XML and DataElement as it is described below: For each DataGroup a XML
+ * tag is created where the tag name is the DataGroups nameInData. The DataGroups attributes are
+ * added as XML attributes, to the DataGroups XML tag. If the DataGroup has a repeatId it is also
+ * added as an attribute with the name repeatId to the groups XML tag.
+ * 
+ * Note that this implies that there can not exist an attribute with the name in data repeatId for
+ * any DataElement.
+ * 
+ * Each DataGroup can have several children which can be either DataGroup or DataAtomic. The
+ * DataAtomic is mapped to a XML with the tag name using nameIndata, and its value using the
+ * DataAtomics value. Only one attribut is mapped from DataAtomic to an XML tag, which is the
+ * repeatId.
+ * 
+ * This module converts both to and from XML.
+ * 
+ * @provides ConverterFactory the ConverterFactory interface which is implemented in this module.
+ */
 module se.uu.ub.cora.xmlconverter {
 	requires se.uu.ub.cora.converter;
 	requires transitive java.xml;
@@ -5,5 +26,4 @@ module se.uu.ub.cora.xmlconverter {
 
 	provides se.uu.ub.cora.converter.ConverterFactory
 			with se.uu.ub.cora.xmlconverter.XmlConverterFactory;
-
 }
