f7132b88d568e685e87f5e7c8197730bf13064d4
pere <pere@ed97f72a7a6f>
2022-09-05 08:58:50 +0000

    Upgrade dependencies

--- src/test/java/se/uu/ub/cora/xmlconverter/spy/DataGroupSpy.java
+++ src/test/java/se/uu/ub/cora/xmlconverter/spy/DataGroupSpy.java
@@ -27,6 +27,7 @@ import java.util.Map;
 import se.uu.ub.cora.data.DataAtomic;
 import se.uu.ub.cora.data.DataAttribute;
 import se.uu.ub.cora.data.DataChild;
+import se.uu.ub.cora.data.DataChildFilter;
 import se.uu.ub.cora.data.DataGroup;
 
 public class DataGroupSpy implements DataGroup {
@@ -205,6 +206,18 @@ public class DataGroupSpy implements DataGroup {
 			String childNameInData, DataAttribute... childAttributes) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<DataChild> getAllChildrenMatchingFilter(DataChildFilter childFilter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeAllChildrenMatchingFilter(DataChildFilter childFilter) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
